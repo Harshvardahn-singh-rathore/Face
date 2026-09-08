3e9016b180d43a9eedc29cd4456e896bd37212d6
Harshvardahn-singh-rathore <harshvardhanrathore720@gmail.com>
2026-09-08 12:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import cv2
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 from insightface.app import FaceAnalysis
